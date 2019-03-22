15adb4396a002402b34247f905c97fe9b8974f8e
Unknown <ryan.martin.gawn@outlook.com>
2019-03-22 17:39:27 +1300

    Initial Commit

---
+++ testComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export class TestComponent extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello my name is Ryan</Text>
+                <Text>My age is 20 :)</Text>
+            </View>
+        )
+    }
+}
